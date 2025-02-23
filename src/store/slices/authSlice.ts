3541e9ce5cac22082ffc13d74823445b2bcfb47e
p1xle <wizmonkey1337@gmail.com>
2025-02-23 18:47:56 +0530

    updated redux slice

--- src/store/slices/authSlice.ts
+++ src/store/slices/authSlice.ts
@@ -2,34 +2,37 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface User {
     uid: string;
-    displayName: string | null;
+    displayName: string;
     email: string | null;
-}
-
-interface AuthState {
+    firstName?: string;
+    lastName?: string;
+  }
+  
+  interface AuthState {
     user: User | null;
     isAuthenticated: boolean;
-}
-
-const initialState: AuthState = {
+  }
+  
+  const initialState: AuthState = {
     user: null,
     isAuthenticated: false,
-}
-
-const authSlice = createSlice({
-    name: 'auth',
+  };
+  
+  const authSlice = createSlice({
+    name: "auth",
     initialState,
     reducers: {
-        setUser: (state, action: PayloadAction<User | null>) => {
-            state.user = action.payload;
-            state.isAuthenticated = !!action.payload;
-        },
-        logout: (state) => {
-            state.user = null;
-            state.isAuthenticated = false
-        }
-    }
-})
-
-export const { setUser, logout } = authSlice.actions;
-export default authSlice.reducer;
+      setUser: (state, action: PayloadAction<User | null>) => {
+        state.user = action.payload;
+        state.isAuthenticated = !!action.payload;
+      },
+      logout: (state) => {
+        state.user = null;
+        state.isAuthenticated = false;
+      },
+    },
+  });
+  
+  export const { setUser, logout } = authSlice.actions;
+  export default authSlice.reducer;
+  
